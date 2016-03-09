53f0219eced433ee42cc710593add389bad3a276
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-03-09 14:03:40 +0100

    Cache database index page

--- correctiv_eurosfueraerzte/urls.py
+++ correctiv_eurosfueraerzte/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 from django.utils.translation import ugettext_lazy as _
 
+from correctiv_community.helpers.cache_utils import cache_page_anonymous as c
+
 from .views import (IndexView, SearchView, DrugDetailView,
                     ObservationalStudyDetailView, CompanyDetailView)
 
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='eurosfueraerzte-index'),
+    url(r'^$', c(IndexView.as_view()), name='eurosfueraerzte-index'),
     url(_(r'^search/$'), SearchView.as_view(), name='eurosfueraerzte-search'),
     url(_(r'^drug/(?P<slug>[\w-]+)/$'), DrugDetailView.as_view(),
         name='eurosfueraerzte-drugdetail'),
