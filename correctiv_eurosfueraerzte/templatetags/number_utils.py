e3b2abeeaba793ad42bcc250a7f68fbbaa890dcf
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-07-12 11:31:57 +0200

    Fix number utils

--- correctiv_eurosfueraerzte/templatetags/number_utils.py
+++ correctiv_eurosfueraerzte/templatetags/number_utils.py
@@ -5,7 +5,7 @@ from django.contrib.humanize.templatetags.humanize import intcomma
 register = template.Library()
 
 
-def intcomma_floatformat(value, arg):
+def intcomma_floatformat(value, arg=2):
     if value is None:
         return None
 
@@ -15,14 +15,18 @@ def intcomma_floatformat(value, arg):
 
     val = intcomma(value)
     if DECIMAL_SEPARATOR not in val:
-        val += '%s%s' % (DECIMAL_SEPARATOR, '0' * arg)
+        if arg > 0:
+            val += '%s%s' % (DECIMAL_SEPARATOR, '0' * arg)
     else:
         before_ds, after_ds = val.rsplit(DECIMAL_SEPARATOR, 1)
         if len(after_ds) > arg:
             after_ds = after_ds[:arg]
         elif len(after_ds) < arg:
             after_ds += '0' * (arg - len(after_ds))
-        val = '%s%s%s' % (before_ds, DECIMAL_SEPARATOR, after_ds)
+        if arg > 0:
+            val = '%s%s%s' % (before_ds, DECIMAL_SEPARATOR, after_ds)
+        else:
+            val = before_ds
     return val
 
 register.filter('intcomma_floatformat', intcomma_floatformat)
