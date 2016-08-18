f33f631ff23c2c863cb799e2f147911246f4d10a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-08-18 16:08:49 +0200

    Change CH project name

--- correctiv_eurosfueraerzte/views.py
+++ correctiv_eurosfueraerzte/views.py
@@ -27,7 +27,7 @@ class LocaleIncludeDict(object):
 class LocaleMixin(object):
     TITLES = {
         'de': _(u'Euros for Doctors'),
-        'ch': _(u'Swiss Francs for Doctors'),
+        'ch': _(u'Money for Doctors'),
     }
 
     def get_context_data(self, **kwargs):
