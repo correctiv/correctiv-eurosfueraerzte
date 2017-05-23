50dd4311c193ee03662462140fb62d3281f65750
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-05-23 21:25:48 +0200

    Move less back into efa project

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
+var exec = require('child_process').exec;
+var combiner = require('stream-combiner2');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+
+var CSS_SOURCE = 'correctiv_eurosfueraerzte/static/eurosfueraerzte/less';
+var CSS_DEST = 'correctiv_eurosfueraerzte/static/eurosfueraerzte/css';
+var MAIN_LESS_FILE = '/eurosfueraerzte.less';
+
+/* Compile, minify, and compress LESS files */
+gulp.task('less', function() {
+  var combined = combiner.obj([
+    gulp.src(CSS_SOURCE + MAIN_LESS_FILE),
+    less(),
+    autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }),
+    gulp.dest(CSS_DEST)
+  ]);
+
+  // any errors in the above streams will get caught
+  // by this listener, instead of being thrown:
+  combined.on('error', console.error.bind(console));
+
+  return combined;
+});
+
+/* Watch Files For Changes */
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch(CSS_SOURCE + '/**/*.less', ['less']);
+
+
+  /* Trigger a live reload on any Django template changes */
+  gulp.watch('**/templates/**').on('change', livereload.changed);
+
+  /* Trigger a live reload upon CSS complilation */
+  gulp.watch(CSS_DEST + '/**').on('change', livereload.changed);
+});
+
+gulp.task('watch', ['less', 'watch']);
+
+/* Create a build of frontend code */
+gulp.task('default', ['less']);
