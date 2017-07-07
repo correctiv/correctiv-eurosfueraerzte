a085571ecd14a9690ff6db497e163a643fb39856
Simon Jockers <hello@simonjockers.com>
2017-07-07 17:27:33 +0200

    Add minimal Leaflet map

--- correctiv_eurosfueraerzte/static/eurosfueraerzte/js/map.js
+++ correctiv_eurosfueraerzte/static/eurosfueraerzte/js/map.js
@@ -1,1 +1,5 @@
-console.log(`Hello ES6!`)
+import L from 'leaflet';
+
+var map = new L.Map('efa__map').setView([12.3, 123.4], 10);
+
+var tiles = new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
