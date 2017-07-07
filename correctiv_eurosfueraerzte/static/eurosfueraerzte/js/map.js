79d25f8fc68f71fdb7e75255e9aa1c809b6fbc53
Simon Jockers <hello@simonjockers.com>
2017-07-07 18:19:34 +0200

    Map: Use CARTO baselayer

--- correctiv_eurosfueraerzte/static/eurosfueraerzte/js/map.js
+++ correctiv_eurosfueraerzte/static/eurosfueraerzte/js/map.js
@@ -1,8 +1,22 @@
 import L from 'leaflet';
 
+const CONTAINER = 'efa__map'
+const TILES_URL = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}@2x.png'
+const TILES_ATTRIBUTION = `
+  © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>,
+  © <a href="https://carto.com/attribution">CARTO</a>`
+
 const initializeMap = ({ center }) => {
-  var map = new L.Map('efa__map').setView(center, 10);
-  var tiles = new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+  var map = new L.Map(CONTAINER, {
+    center: center,
+    scrollWheelZoom: false,
+    zoom: 10
+  });
+
+  new L.TileLayer(TILES_URL, {
+    attribution: TILES_ATTRIBUTION,
+    maxZoom: 18
+  }).addTo(map);
 }
 
 export { initializeMap }
