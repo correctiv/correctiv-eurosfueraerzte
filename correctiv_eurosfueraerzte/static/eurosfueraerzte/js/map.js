8dbef178a9808c83fa3015d1d9f30769665ffdad
Simon Jockers <hello@simonjockers.com>
2017-07-12 19:22:32 +0200

    Customize marker cluster styling to match overall appearance

--- correctiv_eurosfueraerzte/static/eurosfueraerzte/js/map.js
+++ correctiv_eurosfueraerzte/static/eurosfueraerzte/js/map.js
@@ -1,11 +1,12 @@
-import { Map, tileLayer } from 'leaflet';
+import { Map, tileLayer, marker, icon, markerLayer } from 'leaflet';
 import markerClusterGroup from 'leaflet.markercluster';
 
-const CONTAINER = 'efa__map'
-const TILES_URL = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}@2x.png'
+const CONTAINER = 'efa__map';
+const ICON_URL = '/static/eurosfueraerzte/img/map-marker.svg';
+const TILES_URL = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}@2x.png';
 const TILES_ATTRIBUTION = `
   © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>,
-  © <a href="https://carto.com/attribution">CARTO</a>`
+  © <a href="https://carto.com/attribution">CARTO</a>`;
 
 const initializeMap = ({ center, places }) => {
   const map = new Map(CONTAINER, {
@@ -25,9 +26,15 @@ const initializeMap = ({ center, places }) => {
   });
 
   // TODO: Initialize with JSON (to be passed through from initialization)
-  places.forEach(function(c) {
-    markers.addLayer(L.marker(c.geo, {title: c.name})
-                      .bindPopup(`<a href="${c.url}">${c.name}</a>`));
+  places.forEach(c => {
+    markers.addLayer(marker(c.geo, {
+      title: c.name,
+      icon: icon({
+        iconUrl: ICON_URL,
+        iconSize: [20, 30],
+        iconAnchor: [10, 5]
+      })
+    }).bindPopup(`<a href="${c.url}">${c.name}</a>`));
   });
 
   map.addLayer(tiles)
